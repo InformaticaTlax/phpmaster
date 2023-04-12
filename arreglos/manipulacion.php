9cdcd2278f515c9aa0ea53e253989d8e084021e2
InformaticaSegob <dep.informatica.segob@gmail.com>
2023-04-12 17:57:56 -0600

    continuidad

--- arreglos/manipulacion.php
+++ arreglos/manipulacion.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $edaddes = [18,20, 40, 34];
 
 //count
@@ -23,5 +24,44 @@ $tienda = array(
 foreach($tienda as $subindice => $late){
     echo "el costo de cafe $subindice cuesta $$late \n";
 }
-echo "\n";  
+echo "\n"; 
 
+//platzi
+
+function get_pokemon(){
+    $numero_aleatorio = Rand(1,4);
+
+    switch($numero_aleatorio){
+        case 1:
+            echo "pikachu";
+            break;
+        case 2:
+            echo "Charmander";
+            break ;
+        case 3:
+            echo "Mewtwo";
+            break;
+        default:
+            echo "LOL";
+    }
+}
+
+for($i=1;$i<=10;$i++){
+    echo "\n";
+    get_pokemon();
+    echo "<br>";
+}
+*/
+function es_estudainte($platzi_rank){
+    if ($platzi_rank >= 20000){
+        echo "eres un gran esudiante";
+    }
+    else{
+        echo "lo siento";
+
+    }
+}
+
+$entrada = (int) readline("por favor , dinpo tus puntos: ");
+es_estudainte($entrada);
+echo "<br>";
