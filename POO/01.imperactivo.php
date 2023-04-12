d81b18b3255c60540f942c87f6bb5c1910564f80
InformaticaSegob <dep.informatica.segob@gmail.com>
2023-04-12 14:28:29 -0600

    continuando

---
+++ POO/01.imperactivo.php
@@ -0,0 +1,11 @@
+<?php
+    //codigo emperativo o espagueti
+    $automovil1 = (object)["marca"=> "toyota", "modelo"=>"corola"];
+    $automovil2 = (object)["marca"=> "hyundaai", "modelo"=>"Accent"];
+
+    function mostrar($automovil){
+        echo "";
+
+    }
+
+?>
