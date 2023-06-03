28ce0879316b2595b2ce1cb92d406f7f766b0cef
Emanuel corona <duldu35@gmail.com>
2023-06-02 22:56:10 -0600

    aun no se termina el video 46

--- 4.PDO/ajax/formularios.ajax.php
+++ 4.PDO/ajax/formularios.ajax.php
@@ -3,13 +3,31 @@ require_once "../controladores/formularios.controlador.php";
 require_once "../modelos/formulario.modelo.php";
 
 class AjaxFormulario{
-    
+    //validar email existente
+
+    public $validarEmail;
+
+    public function ajaxValidarEmail(){
+
+        $item = "email";
+        $valor = $this->validarEmail;
+
+        $respuesta = ControladorFormularios::ctrSeleccionarRegistros($item, $valor);
+        echo '<pre>';
+        print_r($respuesta);
+        echo'</pre>';
+
+
+    }
 }
 
 //objeto de ajax que recibe la variable post
 
 if(isset($_POST["validarEmail"])){
 
-    
+    $valEmail= new AjaxFormularios();
+    $valEmail -> validarEmail = $_POST["validarEmail"];
+    $valEmail -> ajaxValidarEmail();
+
 
 }
