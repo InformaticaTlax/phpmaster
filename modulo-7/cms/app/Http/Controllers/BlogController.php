6df11c996c9a07a53d5d5f04b679072428d2657d
InformaticaSegob <dep.informatica.segob@gmail.com>
2023-10-12 18:07:30 -0600

    Min 10

--- modulo-7/cms/app/Http/Controllers/BlogController.php
+++ modulo-7/cms/app/Http/Controllers/BlogController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ Blog;
 
 class BlogController extends Controller
 {
     //
+    public function traerBlog(){
+
+        $blog = Blog::all();
+
+        return view("paginas.blog",array("blog"=>$blog));
+        
+    } 
 }
