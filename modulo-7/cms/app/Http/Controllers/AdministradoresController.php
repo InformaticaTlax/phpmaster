799a964332a0c89dc5ea7d2837045506d277b264
Emanuel corona <duldu35@gmail.com>
2023-10-16 22:16:17 -0600

    min 13

--- modulo-7/cms/app/Http/Controllers/AdministradoresController.php
+++ modulo-7/cms/app/Http/Controllers/AdministradoresController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Administradores;
 
 class AdministradoresController extends Controller
 {
     //
+    public function traerAdministradores() {
+        $administradores = Administradores::all();
+
+        return view("paginas.administradores", array("administradores"=> $administradores));
 }
